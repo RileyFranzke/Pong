c1a5628b24e07a50283681c9f8500231d9bd4cfe
RileyFranzke <rsfranzke@franzkeonline.com>
2025-05-27 16:56:20 -0500

    Update pong-index.js

--- javascript/pong-index.js
+++ javascript/pong-index.js
@@ -177,7 +177,7 @@ function updateBall() {
   //Right Wall
   if (newBallPos.x + ball.size.x > game.size.x - game.padding.x) {
     ball.position.x = game.size.x - game.padding.x;
-    ball.acceleration += 0.05;
+    ball.acceleration += 2;
     ball.direction.x = -Math.abs(ball.direction.x + ball.acceleration);
     ball.direction.y += paddleRight.velocity.y * -1 * ball.inertialTransfer;
     ball.direction.y += randomAdjust(1, false);
@@ -206,14 +206,14 @@ function updateBall() {
   }
   //Bottom Wall
   if (newBallPos.y + ball.size.y > game.size.y - game.padding.y) {
-    ball.acceleration += 0.05;
+    ball.acceleration += 2;
     ball.direction.y = -Math.abs(ball.direction.y);
     ballHitWall.turnOn();
   }
   //Left Wall
   if (newBallPos.x < game.padding.x) {
     ball.position.x = game.padding.x;
-    ball.acceleration += 0.05;
+    ball.acceleration += 2;
     ball.direction.x = Math.abs(ball.direction.x - ball.acceleration);
     ball.direction.y += paddleLeft.velocity.y * -1 * ball.inertialTransfer;
     ball.direction.y += randomAdjust(1, false);
@@ -242,7 +242,7 @@ function updateBall() {
   }
   //Top Wall
   if (newBallPos.y < game.padding.y) {
-    ball.acceleration += 0.05;
+    ball.acceleration += 2;
     ball.direction.y = Math.abs(ball.direction.y);
     ballHitWall.turnOn();
   }
